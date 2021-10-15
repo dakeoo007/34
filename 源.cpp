5b72ece8aaf8d6b65e0c3b607f987e8a1b8cdf38
sixducks <82391600+sixducks@users.noreply.github.com>
2021-10-15 23:29:06 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void mian()
+{
+	char c1 = 'A';
+	cout << 'W';
+	cout << c1 << endl;
+	cout << "This is a test." << endl;
+	cout << endl;
+	int n = 100;
+	cout << 10;
+	cout << n;
+	cout << 2 * n << endl;
+	cout << endl;
+	double pi = 3.1415926, r = 10.0, s = pi*r*r;
+	cout << pi << endl;
+	cout << r;
+	cout << s;
+	cout << 2 * r*pi << endl;
+	cout << endl;
+	cout << 'W' << " " << c1 << endl;
+	cout << "This is a Test." << endl;
+	cout << "pi=" << pi << " r=" << r << " s=" << s << endl;
+}
